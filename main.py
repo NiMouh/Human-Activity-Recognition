68411a24d00ebd8dd526e0203a04b703b73fe938
NiMouh <simao.andrade@ubi.pt>
2022-11-22 18:32:33 +0000

    small updates 1.0

--- main.py
+++ main.py
@@ -1,51 +1,66 @@
 # Pratical Project 2: Human Activity Recognition
+# Imports
+import csv
 
 # Declaration of the variable that represents the number of the samples
-number_of_samples = 20
+number_of_samples = 20  # Hz
+
+# Declaration of the labels
+labels = {
+    'Downstairs': '0',
+    'Jogging': '1',
+    'Sitting': '2',
+    'Standing': '3',
+    'Upstairs': '4',
+    'Walking': '5'
+}
 
 
-# Function that receives the name of the file and returns the data of the csv file
-def read_csv_file(file_name):
-    file = open(file_name, 'r')
-    data = file.read()
-    file.close()
+# Function that receives the name of the csv file and returns a list of instances
+def read_csv(file_name):
+    reader = csv.reader(open(file_name, 'r'))
+
+    # Initial data structure
+    data = []
+
+    # Discard the first line
+    next(reader)
+
+    # For each row
+    for row in reader:
+        data.append(row)
+
     return data
 
 
 # Every line received have the following formate: "ID,Activity, timestamp, x, y, z"
 # This function will create an instance with 20 samples of the same activity and same ID
 def create_instance(data, id, activity):
+    # Initial data structure
     instance = []
-    for i in range(number_of_samples):
-        instance.append(data[id][activity][i])
+
+    # Run through the data, if you find a line with the same ID and activity, add it to the instance.
+    # Repeat the process until you got 'number_of_samples' samples
+    for row in data:
+        if row[0] == id and row[1] == activity:
+            # Only the x, y and z values are relevant
+            instance.append([float(row[3]), float(row[4]), float(row[5])])
+            if len(instance) == number_of_samples:
+                break
+
     return instance
 
 
 # Main function
 if __name__ == '__main__':
     # Read the csv file
-    data = read_csv_file('time_series_data_human_activities.csv')
+    data = read_csv('time_series_data_human_activities.csv')
 
-    # Split the data in lines
-    data = data.split('\n')
+    # Print data
+    print(data)
 
-    # Remove the first line of the data
-    data.pop(0)
+    # Create the instance to the ID 36 and the activity "Jogging" (Label 1)
+    instance = create_instance(data, '36', 'Downstairs')
 
-    # Create a dictionary that will have the following structure:
-    # {ID: {Activity: [samples]}}
-    data = {i: {} for i in range(1, 11)}
-    for line in data:
-        line = line.split(',')
-        if line[1] not in data[int(line[0])]:
-            data[int(line[0])][line[1]] = []
-        data[int(line[0])][line[1]].append([float(line[3]), float(line[4]), float(line[5])])
-
-    # Create the instances (create_instance function)
-    instances = []
-    for id in data:
-        for activity in data[id]:
-            instances.append(create_instance(data, id, activity))
-
-    # Print the instances
-    print(instances)
+    # Print the instance
+    print(instance)
